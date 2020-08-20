84118b04fb8c3e61ee5835d58598760b58fd2c25
khizramk02 <khizramsaeed02@gmail.com>
2020-08-20 21:20:45 +0500

    added weather app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Practice from './practice';
-import {Counter} from './counter';
-
-// only one component can be exported default like in the case App component is exported
-// so counter component will not be exported default.
-// we can use classes as variables like
-// className = red ${isHot 'red' : 'blue'}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-  let [count,setCount] = useState(10);
-  let[isHot , setHot] = useState(true);
+  const API_KEY = "c9daf7ccc4a8bfb7ae67296215b8acc7";
+  const base = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${API_KEY}`;
 
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+      "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return `${day}, ${date} ${month} ${year}`;
+  }
+
+  //instead of building a function for date we can use slicing also.
+  // let date = String(new window.Date());
+  // date = date.slice(0, 15);
+
+  const search = evt => {
+    if (evt.key === 'Enter') {
+      fetch(base).then(res => res.json()).then(result => {
+        setQuery('');
+        setWeather(result);
+        console.log(result);
+      });
+    }
+  }
   return (
-    <div className ={`hot ${isHot ? 'hot' : 'cold'}`}>
+    <div className= { (typeof weather.main != "undefined") ? (weather.main.temp >16) ? "App" : "App cold" : "App"}>
+      <main className='search-box'>
+        <h1>Weather App</h1>
+        <input
+          className='search-field'
+          type='text'
+          placeholder='Search for place..'
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
 
-        <h3>Checking whether it is hot or cold outside...<br /> {isHot ? 'yes, Hot' : 'cold'}</h3>
-        <h3>from app.js</h3>
-        <Practice  type = 'react'/>
-        
-        <Practice  type = 'react and html'/>
-        
-        <Counter  counter = {count}/>
-
-        <button onClick={
-          ()=> setCount(++count)
-        }>
-          counter increment
-          </button>
-
-          <button onClick={
-          ()=> setHot(!isHot)
-        }>
-          convert temperature
-          </button>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+              {/* <div className='date'>{date}</div> */}
+              {/* by using the slicing approach */}
+            </div>
+            <div className='weather-box'>
+              <div className='temperature'>{Math.round(weather.main.temp)}°C</div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
--- src/counter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Counter(props){
-    return(
-        <div>
-            <h3>The counter value is: {props.counter}</h3>
-        </div>
-    );
-} 
